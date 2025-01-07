8f690c1ffa6143fa44950d3f360479124877b45e
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-01-07 15:47:40 +0100

    :feelsgood:

--- commands/embed2.lua
+++ commands/embed2.lua
@@ -4,6 +4,14 @@ local client = _G.client
 --[ Authorized User --]
 local authorizedUserID = "995664658038005772"
 
+local member = message.member
+
+local invalidPermsEmbed = {
+    title = "<:WarningRed:1326148101534253107> **Invalid Permission!**",
+    description = "<:RightArrow:1320102028848136294> **Only <@445152230132154380> is allowed to execution this command!**",
+    color = 0xFF0000,
+}
+
 return {
     name = 'embed2',
     description = 'lola',
