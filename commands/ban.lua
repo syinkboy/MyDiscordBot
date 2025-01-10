2eb313cbd2d79444a11b4aa94342c55dea345eff
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-01-10 20:21:18 +0100

    💩

--- commands/ban.lua
+++ commands/ban.lua
@@ -3,27 +3,36 @@ local client = discordia.Client()
 
 client:on('messageCreate', function(message)
   if message.author.bot then return end
+  if not message.guild then return end
 
   if message.content:sub(1, 4) == '!ban' then
     local author = message.guild:getMember(message.author.id)
-    local member = message.mentionedUsers.first
+    local mentionedUser = message.mentionedUsers.first
+    local member = mentionedUser and message.guild:getMember(mentionedUser.id)
 
     if not member then
-      -- The user have not mentioned any member to be banned
       message:reply("Please mention someone to ban!")
       return
-    elseif not author:hasPermission("banMembers") then
-      -- The user does not have enough permissions
+    elseif not author:hasPermission(message.channel, "banMembers") then
       message:reply("You do not have the `banMembers` permissions!")
       return
     end
 
     for user in message.mentionedUsers:iter() do
-      -- Check if mention isn't a reply
-      if string.find(message.content, "<@[!]?" .. user.id .. ">") then
-        member = message.guild:getMember(user.id)
-        if author.highestRole.position > member.highestRole.position then
-          member:ban()
+      if message.content:find("<@!?" .. user.id .. ">") then
+        local memberToBan = message.guild:getMember(user.id)
+        if memberToBan and author.highestRole.position > memberToBan.highestRole.position then
+          local success, err = pcall(function()
+            memberToBan:ban()
+          end)
+
+          if success then
+            message:reply(user.username .. " has been banned!")
+          else
+            message:reply("Failed to ban " .. user.username .. ": " .. tostring(err))
+          end
+        else
+          message:reply("You cannot ban this member!")
         end
       end
     end
