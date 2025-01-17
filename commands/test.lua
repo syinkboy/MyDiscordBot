d4734bd2ad0e26a9e4487a188010c141fb011a18
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-01-17 21:51:23 +0100

    Fixed the ping command,

--- commands/test.lua
+++ commands/test.lua
@@ -1,22 +1,25 @@
 local discordia = _G.discordia
-local client = _G.client'
+local client = _G.client 
 
 local comps = discordia.Components()
 :button{
-  id = "Test",
-  label = "Button!",
-  style = "success",
+    id = 'Test',
+    label = 'Ping!',
+    style = 'danger',
 }
 
-     return {
-        name = 'Test',
-        description = 'Testing the buttons',
-        callback = function(message, args)
-            local sent_msg = message:reply({
-                content = 'DO NOT PRESS',
-                components = comps:raw()
-            })
-            local _, interaction = sent_msg:waitComponent('Buton')
-            interaction:reply('You just pressed the button of death, good job!')
-        end
-    }
+return {
+    name = 'Test',
+    description = 'Replies with pong',
+    callback = function(message, args)
+        local sent_msg = message:reply({
+            content = 'Hell nah',
+            components = comps:raw()
+
+        })
+        local _, interaction = sent_msg:waitComponent('Button')
+        interaction:reply('Ping!')
+    end
+
+        
+}
