d3e12a99607806da8661014303685fc97f5a8e8d
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-04-06 19:35:53 +0200

    Approve update

--- commands/approve.lua
+++ commands/approve.lua
@@ -1,78 +1,66 @@
 local discordia = _G.discordia
 
-local allowedRoleId = "1331790180121841664"
+local allowedRoleId = '1331790180121841664'
 
 return {
-    name = "approve",
-    description = "Direct Message",
+    name = 'approve',
+    description = 'Apprrove a user',
     callback = function(message, args)
         if message.author.bot then return end
 
         local member = message.guild:getMember(message.author.id)
         if not member then
-            return message:reply("Could not fetch your member data.")
+            return message:reply('I could not fetch your member data.')
         end
 
-        local hasRole = false
-        for role in member.roles:iter() do
-            if role.id == allowedRoleId then
+        local hasROle = falsefor role in member.roles:iter() do
+            if role.id == allowedRoleID then
                 hasRole = true
                 break
             end
-        end
 
-        if not hasRole then
-            return message:reply("You do not have permission to use this command.")
-        end
+            if not hasROle then
+                return message:reply('You do not have permission to use this command.')
+                end
 
-        local user = message.mentionedUsers and message.mentionedUsers.first
-        if not user then
-            return message:reply("Please mention a valid user.")
-        end
+                print('Recieved args:', args and table.concat(args, ',') or 'nil')
 
-        print("Original args:", args and table.concat(args, ", ") or "nil")
+                if not args or type(args) ~= 'table' or #args < 3 then
+                    return message:reply('Usage: b!approve @user [Reason]')
+                end
 
-        -- Remove all args before the first non-mention word
-        local mentionIndex = nil
-        for i, arg in ipairs(args) do
-            if arg:match("^<@!?.+>$") then
-                mentionIndex = i
-                break
+                local user = message.mentionedUsers and message.mentionedUser.first
+                if not user then
+                    return message:reply('Please mention a valid user.')
+                end 
+
+                table.remove(args, 1)
+
+                local reason = table.concat(args, '')
+
+                print('User:', user.username)
+                print('Reason:', reason)
+
+                local emb = {
+                    title = 'Staff Application Results',
+                    description = 'Hello,' .. user.username .. '!',
+                    field = {
+                        {name = 'Result', value = 'We are pleased to inform you that you have passed your application. Head to ⁠training-chat for further instructions.', inline = false},
+'}
+                    },
+                    color = 0x50C878,
+                    timestamp = discordia.Date():toISO()
+
+                }
+
+                local success, err = message.channel:send {
+                    content = '<@' .. user.id .. '>',
+                    embed = emb,
+                    allowed_mentions = { allowedRoleID = { Role.id }}
+                }
+
+                if not success then
+                    print('[ERROR] failed to send message:', err)
+                end
             end
-        end
-
-        if mentionIndex then
-            table.remove(args, mentionIndex)
-        end
-
-        if #args < 2 then
-            return message:reply("Usage: !approve @user [reason] [rank]")
-        end
-
-        local rank = args[#args]
-        table.remove(args, #args)
-
-        local reason = table.concat(args, " ")
-
-        local emb = {
-            title = "Promotion Issued",
-            fields = {
-                { name = "User", value = "<@" .. user.id .. ">", inline = true },
-                { name = "Reason", value = reason or "No reason provided", inline = true },
-                { name = "Rank", value = rank or "No rank specified", inline = false }
-            },
-            color = 0xFF0000,
-            timestamp = discordia.Date():toISO()
-        }
-
-        local success, err = message.channel:send {
-            content = "<@" .. user.id .. ">",
-            embed = emb,
-            allowed_mentions = { users = { user.id } }
-        }
-
-        if not success then
-            print("[ERROR] Failed to send message:", err)
-        end
-    end
 }
