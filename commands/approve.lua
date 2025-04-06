958bdde65aed89f632d3178884a6ec95e0d4a3eb
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-04-06 19:43:12 +0200

    s

--- commands/approve.lua
+++ commands/approve.lua
@@ -4,7 +4,7 @@ local allowedRoleId = '1331790180121841664'
 
 return {
     name = 'approve',
-    description = 'Apprrove a user',
+    description = 'Approve a user',
     callback = function(message, args)
         if message.author.bot then return end
 
@@ -13,54 +13,58 @@ return {
             return message:reply('I could not fetch your member data.')
         end
 
-        local hasROle = falsefor role in member.roles:iter() do
-            if role.id == allowedRoleID then
+        local hasRole = false
+        for role in member.roles:iter() do
+            if role.id == allowedRoleId then
                 hasRole = true
                 break
             end
+        end
 
-            if not hasROle then
-                return message:reply('You do not have permission to use this command.')
-                end
+        if not hasRole then
+            return message:reply('You do not have permission to use this command.')
+        end
 
-                print('Recieved args:', args and table.concat(args, ',') or 'nil')
+        print('Received args:', args and table.concat(args, ',') or 'nil')
 
-                if not args or type(args) ~= 'table' or #args < 3 then
-                    return message:reply('Usage: b!approve @user [Reason]')
-                end
+        if not args or type(args) ~= 'table' or #args < 2 then
+            return message:reply('Usage: b!approve @user [Reason]')
+        end
 
-                local user = message.mentionedUsers and message.mentionedUser.first
-                if not user then
-                    return message:reply('Please mention a valid user.')
-                end 
+        local user = message.mentionedUsers and message.mentionedUsers.first
+        if not user then
+            return message:reply('Please mention a valid user.')
+        end 
 
-                table.remove(args, 1)
+        table.remove(args, 1) -- remove mention
 
-                local reason = table.concat(args, '')
+        local reason = table.concat(args, ' ')
 
-                print('User:', user.username)
-                print('Reason:', reason)
+        print('User:', user.username)
+        print('Reason:', reason)
 
-                local emb = {
-                    title = 'Staff Application Results',
-                    description = 'Hello,' .. user.username .. '!',
-                    field = {
-                        {name = 'Result', value = 'We are pleased to inform you that you have passed your application. Head to ⁠training-chat for further instructions.', inline = false},
-'}
-                    },
-                    color = 0x50C878,
-                    timestamp = discordia.Date():toISO()
-
+        local emb = {
+            title = 'Staff Application Results',
+            description = 'Hello, ' .. user.username .. '!',
+            fields = {
+                {
+                    name = 'Result',
+                    value = 'We are pleased to inform you that you have passed your application. Head to ⁠#training-chat for further instructions.',
+                    inline = false
                 }
+            },
+            color = 0x50C878,
+            timestamp = discordia.Date():toISO()
+        }
 
-                local success, err = message.channel:send {
-                    content = '<@' .. user.id .. '>',
-                    embed = emb,
-                    allowed_mentions = { allowedRoleID = { Role.id }}
-                }
+        local success, err = message.channel:send {
+            content = '<@' .. user.id .. '>',
+            embed = emb,
+            allowed_mentions = { users = { user.id } }
+        }
 
-                if not success then
-                    print('[ERROR] failed to send message:', err)
-                end
-            end
+        if not success then
+            print('[ERROR] Failed to send message:', err)
+        end
+    end
 }
