a20fb517719f68852ffcaa64ce5fc988009ed903
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-04-06 18:51:06 +0200

    Up

--- commands/about.lua
+++ commands/about.lua
@@ -6,7 +6,7 @@ return {
     description = 'About!',
     callback = function(message, args)
         local send_msg = message:reply({
-            content = 'Hello, this bot is all about me learning discordia, if you try to do something with the bot and it does not answer on some commands you may not have the right perms for that command. Everyone will have perms to the ping command but some commands may be restricted to certain users. If you have any questions or concerns please feel free to reach out to me on discord.'
+            content = 'nd it does not answer on some commands you may not have the right perms for that command. Everyone will have perms to the ping command but some commands may be restricted to certain users. If you have any questions or concerns please feel free to reach out to me on discord.'
         })
     end
 }
