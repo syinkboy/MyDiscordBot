6aa157ada037413d4b551841bf31109786b8c2b9
syinkboy <159165710+syinkboy@users.noreply.github.com>
2025-04-06 18:47:58 +0200

    Update ping

--- commands/ping.lua
+++ commands/ping.lua
@@ -7,7 +7,7 @@ local client = _G.client
 
 return {
   name = "ping",
-  description = "Pong!",
+  description = "pang!",
   -- slashCommand = slashCommand, -- make it a slashcommand
   aliases = {"pingpong"}, -- this is other names for the ping command, so if you have for example a command called "robloxgroup.lua", if you make an alias called "rg" in your aliases that will work as well.
   callback = function(interaction, args)
